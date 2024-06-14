d177907bd9734810aeb2a8233f933389a7d1f79d
Swetha Charles <swethacharles10@gmail.com>
2024-06-14 18:51:40 +0100

    Plug in client method to query for character by id to app
    Add test for app.

--- packages/service-characters/app.js
+++ packages/service-characters/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const {
   getStarWarsCharacters,
+  getStarWarsCharacter
 } = require('./swapiClient');
 
 const app = express();
@@ -11,6 +12,13 @@ app.use(cors());
 app.get('/characters', async (_, res) => {
   return getStarWarsCharacters()
     .then((characters) => res.send({ results: characters }))
+    .catch((err) => res.status(500).send({ err }));
+});
+
+app.get('/characters/:id', (req, res) => {
+  const id = req.params.id;
+  return getStarWarsCharacter(id)
+    .then((character) => res.send(character))
     .catch((err) => res.status(500).send({ err }));
 });
 
--- packages/service-characters/app.test.js
+++ packages/service-characters/app.test.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const request = require('supertest');
+const nock = require('nock');
 
 const mockSWAPIresponse = require('./__mocks__/mockUtils');
 
@@ -7,7 +8,7 @@ describe('Express server test', () => {
   describe('GET /characters', () => {
     beforeEach(() => {
       jest.clearAllMocks();
-
+      nock.cleanAll();
     });
 
     it('responds with expected number of characters in json format', function (done) {
@@ -26,7 +27,48 @@ describe('Express server test', () => {
     });
 
     it('responds 500 status in case of error', function (done) {
-      jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject());
+      jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.reject());
+
+      request(app)
+        .get('/characters')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(500, done);
+    });
+  });
+
+  describe('GET /characters/:id', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      nock.cleanAll();
+    });
+
+    it('responds with expected character in json format', function (done) {
+      mockSWAPIresponse();
+
+      request(app)
+        .get('/characters/4')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err);
+          expect(res.body).toEqual({
+            name: 'Leia Organa',
+            homeworld: 'Corellia',
+            films: [
+              'A New Hope',
+              'A New Hope',
+              'A New Hope',
+              'A New Hope',
+            ],
+          });
+          done();
+        });
+    });
+
+    it('responds 500 status in case of error', function (done) {
+      jest.spyOn(global, 'fetch').mockImplementationOnce(() => Promise.reject());
 
       request(app)
         .get('/characters')
